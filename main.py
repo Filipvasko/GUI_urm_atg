a3ba81c265dc7adf1abc5942ca3a104be79f2644
Filipvasko <filip.vasko@seznam.cz>
2026-05-28 11:48:45 +0200

    Moving home page

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from PySide6.QtWidgets import QApplication
 from My_sidebar import MySideBar
+from pages.home_page import HomePage
 
 
 # print(f"Cesty k DLL: {os.environ.get('PATH')}")
